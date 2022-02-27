b8859bc2391174133a160a007fe78e0e7b161383
alifsyahputra <alif.syahputra@nexsoft.co.id>
2022-02-27 20:36:55 +0700

    membuat dan mencoba module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alif-github/learn-app-go-modules
+
+go 1.16
+
+require github.com/alif-github/learn-go-modules v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alif-github/learn-go-modules"
+)
+
+func main() {
+	fmt.Println(learn_go_modules.SayHelloWorld("Alif"))
+}
