75244013701c459e25d2fa0142065b8901710143
alifsyahputra <alif.syahputra@nexsoft.co.id>
2022-02-27 21:12:05 +0700

    update main

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/alif-github/learn-go-modules"
+	"github.com/alif-github/learn-go-modules/v2"
 )
 
 func main() {
-	fmt.Println(learn_go_modules.SayHelloWorld("Alif"))
+	fmt.Println(learn_go_modules.SayHelloWorld("Bandung", "Jakarta"))
 }
